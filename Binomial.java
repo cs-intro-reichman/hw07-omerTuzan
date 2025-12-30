0274c20dc2e830523188fcb0ed3337a2bb0454c7
admin <omeruzan14@gmail.com>
2025-12-30 14:50:05 +0200

    HW7v0.02: 'changed integer to long'

--- Binomial.java
+++ Binomial.java
@@ -4,7 +4,7 @@ public class Binomial {
 		//// Uncomment the version of binomial that you want to test
  
 		// Testing the basic binomial implementation:
-    	System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    	// System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 
 		// Testing the optimized binomial implementation:
 		System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
@@ -19,12 +19,12 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		//// This function creates a 2D array, say memo, 
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
 		//// Replace the following statement with your code.
-		int [][] memo = new int[n + 1][k + 1];
+		long [][] memo = new long[n + 1][k + 1];
 		for (int i = 0; i < memo.length; i++) {
 			for (int j = 0; j < memo[0].length; j++) {
 				memo[i][j] = -1;
@@ -34,7 +34,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
